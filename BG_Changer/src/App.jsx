4ada05d4dbf21739612130074aa55e1af0d6366a
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2026-06-23 11:01:21 +0530

    feat: initialize Password Generator project with React, Vite, and Tailwind CSS
    - Added main application structure with App component and entry point (main.jsx)
    - Integrated Tailwind CSS for styling with index.css
    - Created SVG icons for various social platforms and documentation
    - Implemented basic styling for components in App.css
    - Set up Vite configuration for React and Tailwind CSS
    - Added hero image and React logo assets

---
+++ BG_Changer/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-700 text-3xl'>Chai aur Code</h1>
+    </>
+  )
+}
+
+export default App
