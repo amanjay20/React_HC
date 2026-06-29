d2a3734d4874e915be48f80ac80fd442f9d25ac1
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2026-06-29 21:31:31 +0530

    feat: add Github and User components with routing and data fetching

--- Router_Project/src/main.jsx
+++ Router_Project/src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+// import about from './components/About_us/About.jsx'
 import './index.css'
-import App from './App.jsx'
+import About from './components/About_us/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
+// import App from './App.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = '/' element = {<Layout />}>
+      <Route path ="" element = {<Home/>}/>
+      <Route path ="about" element = { <About/> }/>
+      <Route path ="contact" element = { <Contact/> }/>
+      <Route path ="user/:userid" element = { <User/> }/>
+      <Route loader={githubInfoLoader}
+      path ="github" 
+      element = { <Github/> }/>
+
+
+
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
