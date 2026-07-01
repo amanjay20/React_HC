c6e8465d4d9652d3c71aefb838e8a3b9b289e7a8
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2026-07-01 19:14:30 +0530

    chore: comment out unused React imports in Github and User components

--- Router_Project/src/components/Github/Github.jsx
+++ Router_Project/src/components/Github/Github.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
@@ -22,6 +22,7 @@ function Github() {
 
 export default Github
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/hiteshchoudhary')
     return response.json()
